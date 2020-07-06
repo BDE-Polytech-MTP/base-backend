da585681d48ff3f8ee5f680c6aacc90ec70f7397
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-07-06 22:17:11 +0200

    Export permissionsFromStrings

--- src/lib.ts
+++ src/lib.ts
@@ -3,3 +3,4 @@ export * from './services';
 export * from './controllers';
 export * from './utils/hash';
 export * from './utils/http-code';
+export { permissionsFromStrings } from './utils/permissions';
