29d653cb5c16d6b730348fdef32a3f1bb21cbe36
Florent HUGOUVIEUX <florent.hugouvieux@protonmail.com>
2020-08-18 15:15:05 +0200

    Change bookings service methods signatures

--- src/services/booking.service.ts
+++ src/services/booking.service.ts
@@ -1,4 +1,4 @@
-import { Booking } from "../models/booking.model";
+import { Booking, Event } from "../models";
 
 export enum BookingsErrorType {
     EVENT_NOT_EXISTS,
@@ -28,21 +28,21 @@ export interface BookingsService {
      * @throws BOOKING_NOT_EXISTS if no booking matching the request exists
      * @throws INTERNAL in any other case
      */
-    findOne(userUUID: string, eventUUID: string): Promise<Booking>;
+    findOne(userUUID: string, eventUUID: string): Promise<Booking & Event>;
 
     /**
      * Finds all bookings for the event with the given UUID.
      * 
      * @param eventUUID The UUID of the event to find bookings for
      */
-    findBookingsForEvent(eventUUID: string): Promise<Booking[]>;
+    findBookingsForEvent(eventUUID: string): Promise<(Booking & Event)[]>;
 
     /**
      * Finds all bookings of the user with the given UUID.
      * 
      * @param userUUID The UUID of the user to find bookings of
      */
-    findBookingsForUser(userUUID: string): Promise<Booking[]>;
+    findBookingsForUser(userUUID: string): Promise<(Booking & Event)[]>;
 
 }
 
